e2811e8e5bc5531595aba520c429895b7a63264a
mwalusha <wugangafransisca@gmail.com>
2026-07-23 16:33:55 +0300

    Add professional landing page

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("terms/", views.terms, name="terms"),
     path("privacy/", views.privacy, name="privacy"),
 ]
